55be2fdd1b7f9d8c2dc905231f6e03caf6993d12
Dreun <thomastvd@gmail.com>
2016-02-23 11:01:54 +0100

    circle fragment

--- app/src/main/java/com/example/kandoe/DrawableGraphics/BulletPoint.java
+++ app/src/main/java/com/example/kandoe/DrawableGraphics/BulletPoint.java
@@ -9,6 +9,7 @@ import android.graphics.RectF;
 import android.graphics.drawable.shapes.OvalShape;
 import android.graphics.drawable.shapes.Shape;
 import android.view.View;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -16,9 +17,14 @@ import java.util.Random;
  * Created by Thomas on 2016-02-22.
  */
 public class BulletPoint extends View {
-    Paint paint = new Paint();
-    int left;int top; int right; int bottom;int color;
-    private View view;
+    private Paint paint;
+    int left;
+    int top;
+    int right;
+    int bottom;
+    int color;
+    private RoundedRectangle trede;
+    private RectF rectF;
 
     public BulletPoint(Context context) {
         super(context);
@@ -34,27 +40,52 @@ public class BulletPoint extends View {
         this.color = color;
     }
 
+
     public BulletPoint(Context context, View trede) {
         super(context);
-        this.view = trede;
+        this.trede = (RoundedRectangle) trede;
+
+
+        initValues();
+
+    }
+
+    private void initValues() {
+
+        //Paint init, layertype is needed for shadow
+        paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        this.setLayerType(LAYER_TYPE_SOFTWARE, paint);
+        paint.setColor(Color.BLUE);
+        paint.setShadowLayer(4.0f, 0.0f, 2.0f, Color.BLACK);
+
+        int size = 40;
+
+        Random random = new Random();
+
+        //Position bullet
+        //int yCenter = this.trede.getBottom2() - this.trede.getTop2();
+
+        int temp = trede.getRight2() - trede.getLeft2() - size * 2;
+        int xCenter = random.nextInt(temp) + trede.getLeft2()+100;
+        int yCenter = random.nextInt(trede.getBottom2()-trede.getTop2())+trede.getTop2();
+
+
+        Rect rect = new Rect(xCenter - size, yCenter - size, xCenter + size, yCenter + size);
+
+        //RectF for rounded corners
+        rectF = new RectF(rect);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
 
-        paint.setColor(Color.BLACK);
 
 
-//verplaatsen naar andere methode
-        int x = view.getLeft()+50;
-        int y = view.getTop()-view.getBottom();
 
-
-        Rect rect = new Rect(x-40,y-40,x+50,y+60);
-        //Rect rect = new Rect(getLeft(),top,right,bottom);
-        RectF rectF = new RectF(rect);
-       canvas.drawRoundRect(rectF, 2, 2, paint);
+        canvas.drawRoundRect(rectF, 100, 100, paint);
 
 
     }
+
+
 }
